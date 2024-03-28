291c061e52b385b82ec29f454b7807c7148a9686
andy9310 <r12921a25@ntu.edu.tw>
2024-03-28 19:06:00 +0800

    finish part1 and part2 function

--- hw1_material/part2/JBF.py
+++ hw1_material/part2/JBF.py
@@ -16,30 +16,54 @@ class Joint_bilateral_filter(object):
         padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE).astype(np.int32)
 
         ### TODO ###
-        output = np.zeros(img.shape, np.float32)
+        output = np.zeros(img.shape, np.uint8)
         y, x = np.mgrid[-self.pad_w:self.pad_w+1, -self.pad_w:self.pad_w+1]
-        g_spatial = np.exp(-(x**2 + y**2) / (2 * self.sigma_s**2))
         print(x)
         print("y:")
         print(y)
+        g_spatial = np.exp(-(x**2 + y**2) / (2 * self.sigma_s**2))
         for i in range(img.shape[0]):
             for j in range(img.shape[1]):
+                
+                i_padded, j_padded = i+self.pad_w , j+self.pad_w
+                # patch_img = padded_img[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1]
+                patch_img_r = padded_img[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,0]
+                patch_img_g = padded_img[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,1]
+                patch_img_b = padded_img[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,2]
+                
+                if padded_guidance.ndim == 3: # rgb
                 # Extract local regions
-                i_padded, j_padded = i + self.pad_w, j + self.pad_w
-                patch_img = padded_img[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1]
-                patch_guidance = padded_guidance[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1]
-
-                # Compute Gaussian range weights
-                d_intensity = patch_guidance - padded_guidance[i_padded, j_padded]
-                g_range = np.exp(-(d_intensity**2) / (2 * self.sigma_r**2))
-
+                    patch_guidance_r = padded_guidance[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,0]
+                    patch_guidance_g = padded_guidance[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,1]
+                    patch_guidance_b = padded_guidance[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1,2]
+                    d_intensity_r = patch_guidance_r - padded_guidance[i_padded, j_padded,0]
+                    d_intensity_g = patch_guidance_g - padded_guidance[i_padded, j_padded,1]
+                    d_intensity_b = patch_guidance_b - padded_guidance[i_padded, j_padded,2]
+                    g_range = np.exp(-(( d_intensity_r/255) **2 + ( d_intensity_g/255) **2 + ( d_intensity_b/255) **2 ) / (2 * self.sigma_r**2))
+                    
+                    
+                else:
+                    patch_guidance = padded_guidance[i_padded-self.pad_w:i_padded+self.pad_w+1, j_padded-self.pad_w:j_padded+self.pad_w+1]
+                    d_intensity = patch_guidance - padded_guidance[i_padded, j_padded]
+                    g_range = np.exp(-(( d_intensity/255) **2) / (2 * self.sigma_r**2))
+                
                 # Calculate the weights
+                # for k in range(3):
                 weights = g_spatial * g_range
-                weighted_sum = np.sum(patch_img * weights)
+                weighted_sum_r = np.sum(patch_img_r * weights)
+                weighted_sum_g = np.sum(patch_img_g * weights)
+                weighted_sum_b = np.sum(patch_img_b * weights)
                 sum_weights = np.sum(weights)
 
-                # Assign the computed value to the output image
-                output[i, j] = weighted_sum / sum_weights
+                    # Assign the computed value to the output image
+                output[i, j, 0] = weighted_sum_r / sum_weights
+                output[i, j, 1] = weighted_sum_g / sum_weights
+                output[i, j, 2] = weighted_sum_b / sum_weights
+                
+                
+                    
+                
+                
 
 
 
